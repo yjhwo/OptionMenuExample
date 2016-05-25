e0a5085ef237f13fe2abd1f83362819d1998324c
kickscar <kickscar@gmail.com>
2016-05-26 01:08:04 +0900

    2nd commit

---
+++ app/src/main/java/com/estsoft/optionmenuexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.estsoft.optionmenuexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
